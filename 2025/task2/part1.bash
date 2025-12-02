941b33e5f3c98d259b2aaf19042bba07acf8aeba
Marek Nagy <marek.nagy@outreach.io>
2025-12-02 17:25:25 +0100

    formatting

--- 2025/task2/part1.bash
+++ 2025/task2/part1.bash
@@ -1,20 +1,17 @@
 #!/usr/bin/env bash
 
-# shellcheck source=../lib.bash
-source ../../lib.bash
 regex='^\([1-9][0-9]*\)\1$'
 
 counter=0
 while read -r -d ',' range
 do
-    echo $range
+    printf "Processing %s\n" "$range" # logging
     nums=(${range//-/ })
     start=${nums[0]}
     end=${nums[1]}
-
     
+    #?: faster then "for" loop over individual numbers, not sure why
     repeating_nums=$(seq -f %1.0f "$start" "$end" | grep "$regex")
-    # echo "Repeating numbers in range $range: $repeating_nums"
     for val in $repeating_nums
     do
         ((counter+=val))
--- 2025/task2/part2.bash
+++ 2025/task2/part2.bash
@@ -1,19 +1,16 @@
 #!/usr/bin/env bash
 
-# shellcheck source=../lib.bash
-source ../../lib.bash
 regex='^\([1-9][0-9]*\)\1\+$'
 
 counter=0
-while read -r -d ',' range
+while read -r -d ',' range # expects "," at the end of the line
 do
-    echo $range
+    printf "Processing %s\n" "$range" # logging
     nums=(${range//-/ })
     start=${nums[0]}
     end=${nums[1]}
     
     repeating_nums=$(seq -f %1.0f "$start" "$end" | grep "$regex")
-    # echo "Repeating numbers in range $range: $repeating_nums"
     for val in $repeating_nums
     do
         ((counter+=val))
