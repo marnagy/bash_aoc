46d5608b8f88db59c3cdf7b7c4a2feadda0ed2ef
Marek Nagy <marek.nagy123456@gmail.com>
2025-12-25 23:16:14 +0000

    [WIP] creating line

--- 2025/task9/part2.bash
+++ 2025/task9/part2.bash
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-abs() {
-    local num=$1
-
-    if [[ "$num" -lt 0 ]]
-    then
-        ((num=-num))
-    fi
-
-    echo "$num"
-}
-
 compute_area() {
     local x1=$1
     local y1=$2
@@ -25,24 +14,75 @@ compute_area() {
     echo "$area"
 }
 
+create_line() {
+    local point1_index=$1
+    local point2_index=$2
+
+    IFS=',' read -r -a point1_nums < "point_${point1_index}.tmp"
+    IFS=',' read -r -a point2_nums < "point_${point2_index}.tmp"
+
+    # check if horizontal or vertical
+
+    # for each point that is not RED, create a file with "LINE"
+    local coord_index=0 # horizontal
+    local other_coord_index=1
+    if [[ "${point1_nums[0]}" -eq "${point2_nums[0]}" ]]
+    then
+        coord_index=1
+        other_coord_index=0
+    fi
+
+    local const_coord="${point1_nums[$other_coord_index]}"
+
+    echo "Difference coord: ${coord_index}" >> /dev/stderr
+    local point1_num="${point1_nums[$coord_index]}"
+    local point2_num="${point2_nums[$coord_index]}"
+    local coord_diff=$(( point1_num - point2_num ))
+    echo "Coord diff: ${coord_diff}" >> /dev/stderr
+
+    local index_multiplier="1"
+    if [[ "$coord_diff" -lt 0 ]]
+    then
+        index_multiplier="-1"
+    fi
+
+    # !: wrong condition
+    # ?: Why though?
+    for ((i=1; i<coord_diff-1; i+1))
+    do
+        if [[ "$coord_index" -eq 0 ]]
+        then
+            green_point_num=$(( point1_nums[coord_index] + i * index_multiplier ))
+            echo "Green point coords: ${green_point_num}-${const_coord}" >> /dev/stderr
+        fi
+    done
+}
+
 line_index=0
 while read -r line
 do
     echo -n "$line" > "point_${line_index}.tmp"
+    echo -n "RED" > "${line}.tmp"
 
     ((line_index++))
 done < "${1:-/dev/stdin}"
 
 
-echo "Drawing map..." >> /dev/stderr
-current_max_area=0
+echo "Connecting red points..." >> /dev/stderr
 for ((i=0; i<line_index; i++))
 do
-    echo
+    next_point_index=$((i+1))
+    if [[ "$next_point_index" -eq "$line_index" ]]
+    then
+        next_point_index=0
+    fi
+
+    # "connect" points
+    create_line "$i" "$next_point_index"
 done
 
-rm ./*.tmp
+# rm ./*.tmp
 
 # the rectangle is inside if all points on the perimeter of it are inside.
 
-echo "Max area found: ${current_max_area}" >> /dev/stderr
+# echo "Max area found: ${current_max_area}" >> /dev/stderr
